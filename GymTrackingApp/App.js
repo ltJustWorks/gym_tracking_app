dfa843dfcabad75b37346b2726d1d273939917d4
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2022-12-26 01:20:13 -0500

    init react native app

---
+++ GymTrackingApp/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>My Gym Tracker</Text>
+      <Button title="Add Exercise" />
+      <Button title="View Exercise List" />
+    </View>
+  );
+};
+
+export default App;
