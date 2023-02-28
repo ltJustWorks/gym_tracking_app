525ac0b1ce04a7f29b61e3f41da30e2358409e3c
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-02-27 19:06:27 -0500

    add delete exercise button

--- GymTrackingApp/screens/ExerciseList.js
+++ GymTrackingApp/screens/ExerciseList.js
@@ -1,28 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import { View, FlatList, Text, Button } from 'react-native'
 import { useIsFocused } from '@react-navigation/native'
-import { getData } from '../storage/dataHelper'
+import { getData, saveData } from '../storage/dataHelper'
 import styles from '../styles/styles'
 
-const ExerciseItem = ({name, sets, reps, weight}) => {
+const onRemoveExercise = (exerciseName, exerciseList, setExerciseList) => {
+    let newExerciseList = JSON.parse(JSON.stringify(exerciseList))
+    const updatedList = newExerciseList.filter(exercise => exercise.name !== exerciseName)
+    saveData("exercises", updatedList)
+        .then(setExerciseList(updatedList))
+        .catch(e => console.error(e))
+}
+
+const ExerciseItem = ({name, sets, reps, weight, exerciseList, setExerciseList}) => {
     return (
         <View style={styles.exerciseContainer}>
             <Text style={styles.itemtitle}>{name}</Text>
             <Text style={styles.subtext}>{sets} sets</Text>
             <Text style={styles.subtext}>{reps} reps</Text>
             <Text style={styles.subtext}>{weight} lbs</Text>
+            <Button
+                title="Remove exercise"
+                onPress={() => onRemoveExercise(name, exerciseList, setExerciseList)} //TODO: Reimplement with id instead
+            />
         </View>
     )
 }
 
-const renderExerciseItem = ({item}) => {
+const renderExerciseItem = (item, exerciseList, setExerciseList) => {
     return (
         <ExerciseItem 
             name={item.name} 
             sets={item.sets} 
             reps={item.reps}
             weight={item.weight}
+            exerciseList={exerciseList}
+            setExerciseList={setExerciseList}
         />
+    )
+}
+
+const NewExerciseButton = ({navigation}) => {
+    return (
+            <Button
+                title="Add New Exercise"
+                onPress={() => {navigation.navigate("NewExerciseForm")}}
+            />
     )
 }
 
@@ -38,17 +61,23 @@ const ExerciseList = ({navigation}) => {
         })
     }, [isFocused])
 
+    if (exerciseList.length === 0) {
+        return (
+            <View>
+                <Text style={styles.itemtitle}>No exercises, add some to start</Text>
+                <NewExerciseButton navigation={navigation} />
+            </View>
+        )
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Exercises</Text>
             <FlatList
                 data={exerciseList}
-                renderItem={renderExerciseItem}
+                renderItem={({item}) => renderExerciseItem(item, exerciseList, setExerciseList)}
             />
-            <Button
-                title="Add New Exercise"
-                onPress={() => {navigation.navigate("NewExerciseForm")}}
-            />
+            <NewExerciseButton navigation={navigation} />
         </View>
     )
 }
