e95c80d998d1e11611e4330480f4583ca37d47dc
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-05-27 10:04:55 -0400

    separate AccordionItem as component

---
+++ GymTrackingApp/components/AccordionItem.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+
+const AccordionItem = ({ children, title }) => {
+  const [ expanded, setExpanded ] = useState(false);
+
+  function toggleItem() {
+    setExpanded(!expanded);
+  }
+
+  const body = <ScrollView style={{flex:1}}><Text style={{fontSize:18}}>{ children }</Text></ScrollView>;
+
+  return (
+    <View style={{flex:1, margin:10}}>
+      <TouchableOpacity 
+        style={{flexDirection:"row", justifyContent:"space-between",
+               padding:5, backgroundColor:"#eaeaea"}} 
+        onPress={ toggleItem }>
+        <Text style={{fontSize: 21}}>{ title }</Text>
+        <Icon name={ expanded ? 'chevron-up' : 'chevron-down' }
+              size={20} color="#bbb"
+        />
+      </TouchableOpacity>
+      { expanded && body }
+    </View>
+  );
+}
+
+export default AccordionItem
--- GymTrackingApp/screens/ViewExercise.js
+++ GymTrackingApp/screens/ViewExercise.js
@@ -4,31 +4,7 @@ import { getData, saveData } from '../storage/dataHelper'
 import styles from '../styles/styles'
 import LinearGradient from 'react-native-linear-gradient'
 import Icon from 'react-native-vector-icons/FontAwesome'
-
-const AccordionItem = ({ children, title }) => {
-  const [ expanded, setExpanded ] = useState(false);
-
-  function toggleItem() {
-    setExpanded(!expanded);
-  }
-
-  const body = <ScrollView style={{flex:1}}><Text style={{fontSize:18}}>{ children }</Text></ScrollView>;
-
-  return (
-    <View style={{flex:1, margin:10}}>
-      <TouchableOpacity 
-        style={{flexDirection:"row", justifyContent:"space-between",
-               padding:5, backgroundColor:"#eaeaea"}} 
-        onPress={ toggleItem }>
-        <Text style={{fontSize: 21}}>{ title }</Text>
-        <Icon name={ expanded ? 'chevron-up' : 'chevron-down' }
-              size={20} color="#bbb"
-        />
-      </TouchableOpacity>
-      { expanded && body }
-    </View>
-  );
-}
+import AccordionItem from '../components/AccordionItem'
 
 const ViewExercise = ({route, navigation}) => {
     const exerciseObj = route.params.exercise
