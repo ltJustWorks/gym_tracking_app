0bb9dcc5933661529a36a5bbaa38a3c23035d690
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-06-25 15:50:49 -0400

    add data display to workout record

--- GymTrackingApp/screens/ViewWorkoutRecord.js
+++ GymTrackingApp/screens/ViewWorkoutRecord.js
@@ -5,6 +5,28 @@ import { getData, saveData } from "../storage/dataHelper"
 import styles from "../styles/styles"
 import AccordionItem from "../components/AccordionItem"
 
+const Set = ({set}) => {
+    return (
+        <View>
+            <Text>{set.weight} lbs</Text>
+            <Text>{set.reps} reps</Text>
+        </View>
+    )
+}
+
+const Exercise = ({exercise_name, set_list}) => {
+    console.log("marker", set_list)
+    return (
+        <View>
+            <Text>{exercise_name}</Text>
+            <FlatList
+                data={set_list}
+                renderItem={({item}) => <Set set={item}/>}
+            />
+        </View>
+    )
+}
+
 const ViewWorkoutRecord = ({route, navigation}) => {
     const {date} = route.params
     const [record, setRecord] = useState({})
@@ -13,13 +35,16 @@ const ViewWorkoutRecord = ({route, navigation}) => {
         getData('workout_history')
             .then((val) => {
                 setRecord(val[date])
-                console.log(Object.entries(val[date]))
+                console.log(val[date])
             })
     }, [])
 
     return (
         <View>
-            <Text>test</Text>
+            <FlatList
+                data={Object.keys(record)}
+                renderItem={({item}) => <Exercise exercise_name={item} set_list={record[item]} />}
+            />
         </View>
     )
 }
