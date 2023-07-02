592fde1e446d53de55cba4182e64bebb5f4a7104
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-07-02 19:55:05 -0400

    fix upgrade issue, new screen

---
+++ GymTrackingApp/screens/EditTemplateForm.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useId } from 'react'
+import { View, Text, TextInput, Button, FlatList, TouchableOpacity, Alert } from 'react-native'
+import { getData, saveData } from '../storage/dataHelper'
+//import { v4 as uuidv4 } from 'uuid'
+import styles from '../styles/styles'
+const exercisesList = require('../data/exercises/exercises.json')
+import Icon from 'react-native-vector-icons/FontAwesome'
+
