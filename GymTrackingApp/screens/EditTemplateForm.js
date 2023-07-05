4ac4a9e06190ad5885582294abc192029a8707e2
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-07-04 22:32:59 -0400

    add components to edit template screen

--- GymTrackingApp/screens/EditTemplateForm.js
+++ GymTrackingApp/screens/EditTemplateForm.js
@@ -3,15 +3,28 @@ import { View, Text, TextInput, Button, FlatList, TouchableOpacity, Alert } from
 import { getData, saveData } from '../storage/dataHelper'
 //import { v4 as uuidv4 } from 'uuid'
 import styles from '../styles/styles'
-const exercisesList = require('../data/exercises/exercises.json')
+const exerciseList = require('../data/exercises/exercises.json')
 import Icon from 'react-native-vector-icons/FontAwesome'
+import ExerciseList from '../components/ExerciseList'
+import AddedExercises from '../components/AddedExercises'
+import SaveTemplateButton from '../components/SaveTemplateButton'
+import SetTemplateName from '../components/SetTemplateName'
+
+const onSaveTemplate = () => {}
 
 const EditTemplateForm = ({route, navigation}) => {
     const {templateObj} = route.params
+    const [templateEditObj, setTemplateEditObj] = useState(templateObj)
 
     return (
-        <View>
-            <Text>{templateObj ? "Yes" : "No"}</Text>
+        <View style={{flex:1, justifyContent:"space-between"}}>
+            <SetTemplateName templateObj={templateEditObj} setTemplateObj={setTemplateEditObj} />
+
+            <ExerciseList exerciseList={exerciseList} templateObj={templateObj} setTemplateObj={setTemplateEditObj} />
+
+            <AddedExercises templateObj={templateEditObj} setTemplateObj={setTemplateEditObj} />
+            <SaveTemplateButton templateObj={templateEditObj} 
+                navigation={navigation} onSaveTemplate={onSaveTemplate}/>
         </View>
     )
 }
