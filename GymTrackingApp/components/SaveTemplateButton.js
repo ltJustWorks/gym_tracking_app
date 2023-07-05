7d8bbda9943abaef362c2b717ae607d921617cee
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-07-04 21:53:06 -0400

    refactoring

--- GymTrackingApp/components/SaveTemplateButton.js
+++ GymTrackingApp/components/SaveTemplateButton.js
@@ -1,31 +1,10 @@
 import React, { useState, useEffect, useId } from 'react'
 import { View, Text, TextInput, Button, FlatList, TouchableOpacity, Alert } from 'react-native'
-import { getData, saveData } from '../storage/dataHelper'
 //import { v4 as uuidv4 } from 'uuid'
 import styles from '../styles/styles'
 const exercisesList = require('../data/exercises/exercises.json')
 
-const onSaveTemplate = (templateObj, navigation) => {
-    if (templateObj.name === "") {
-        Alert.alert("Error", "Add a template name.")
-        return
-    }
-    getData("templates")
-        .then((templatesList) => {
-            // check and alert if another template has same name
-            //for (const template of templatesList) {
-            //}
-            return [...templatesList, templateObj]
-        })
-        .then((newTemplatesList) => {
-            //console.log("new templates list:", newTemplatesList)
-            saveData("templates", newTemplatesList)
-        })
-        .then(() => navigation.navigate("Exercise Template List"))
-
-}
-
-const SaveTemplateButton = ({templateObj, navigation}) => {
+const SaveTemplateButton = ({templateObj, navigation, onSaveTemplate}) => {
     return (
             <View style={{borderRadius:20, overflow:"hidden", margin:4}}><Button 
                 title="Save template"
