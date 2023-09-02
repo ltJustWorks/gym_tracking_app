0df12e632ce2714fd9d7c949a6969a4379dde4d3
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-09-02 15:19:46 -0400

    clean up

--- GymTrackingApp/helpers/helpers.js
+++
@@ -1,7 +0,0 @@
-const formatISODateStr = (ISODateStr) => {
-    const date = new Date(ISODateStr)
-    const formattedDate = date.toLocaleString("en-US", { month: "long", day: "numeric" })
-    return formattedDate
-}
-
-export { formatISODateStr }
